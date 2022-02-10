Fixed bug in assembly for scaling of rectangle. Bug was in quadrature for quadrilaterals

base_assembly.py
@@ -6,7 +6,7 @@ based on Specialization-Project-fall-2021
 
 import numpy as np
 
-from gauss_quadrature import quadrature2D_tri, line_integral_with_basis, quadrature2D_rec
+from gauss_quadrature import quadrature2D_tri, line_integral_with_basis, quadrature2D_quad
 from helpers import inv_index_map, expand_index
 
 
@@ -96,14 +96,14 @@ def assemble_f_local_tri(ck, f_func, p1, p2, p3):
     return f_local
 
 
-def get_basis_coef_rec(p_vec):
+def get_basis_coef_quad(p_vec):
     """
-    Calculate the basis function coef. matrix. for rectangle element
+    Calculate the basis function coef. matrix. for quadrilateral element
 
     Parameters
     ----------
     p_vec : np.array
-        vertexes of the rectangle element.
+        vertexes of the quadrilateral element.
 
     Returns
     -------
@@ -116,9 +116,9 @@ def get_basis_coef_rec(p_vec):
     return np.linalg.inv(mk)  # here faster than solving Mk @ Ck = I_4
 
 
-def phi_rec(x, y, ck, i):
+def phi_quad(x, y, ck, i):
     """
-    The linear basis functions on a rectangle
+    The linear basis functions on a quadrilateral
 
     Parameters
     ----------
@@ -149,9 +149,9 @@ def phi_rec(x, y, ck, i):
     return ck[0, i] + ck[1, i] * x + ck[2, i] * y + ck[3, i] * x * y
 
 
-def ddx_phi_rec(x, y, ck, i):
+def ddx_phi_quad(x, y, ck, i):
     """
-    The linear basis functions on a rectangle
+    The linear basis functions on a quadrilateral
 
     Parameters
     ----------
@@ -182,9 +182,9 @@ def ddx_phi_rec(x, y, ck, i):
     return ck[1, i] + ck[3, i] * y
 
 
-def ddy_phi_rec(x, y, ck, i):
+def ddy_phi_quad(x, y, ck, i):
     """
-    The linear basis functions on a rectangle
+    The linear basis functions on a quadrilateral
 
     Parameters
     ----------
@@ -246,9 +246,9 @@ def assemble_f_local_rec(ck, f_func, p1, p2, p3, p4):
         i, di = inv_index_map(ki)
 
         def f_phi(x, y):
-            return f_func(x, y)[:, di] * phi_rec(x, y, ck, i)
+            return f_func(x, y)[:, di] * phi_quad(x, y, ck, i)
 
-        f_local[ki] = quadrature2D_rec(p1, p2, p3, p4, f_phi, 4)
+        f_local[ki] = quadrature2D_quad(p1, p2, p3, p4, f_phi, 4)
     return f_local
 
 

gauss_quadrature.py
@@ -178,7 +178,7 @@ def get_area_triangle(p1, p2, p3):
     return 0.5 * abs(det_jac)
 
 
-def quadrature2D_rec(p1, p2, p3, p4, g, nq_x, nq_y=None):
+def quadrature2D_quad(p1, p2, p3, p4, g, nq_x, nq_y=None):
     """
     Integral over the triangle with vertices in p1, p2 and p3
 
@@ -208,10 +208,10 @@ def quadrature2D_rec(p1, p2, p3, p4, g, nq_x, nq_y=None):
     if nq_y is None:
         nq_y = nq_x
     # Weights and gaussian quadrature points
-    z, rho = get_points_and_weights_quad_2D_rec(nq_x, nq_y)
+    z, rho = get_points_and_weights_quad_2D_quad(nq_x, nq_y)
 
     # Calculating the Gaussian quadrature summation formula
-    return get_area_quadrilateral(p1, p2, p3, p4) * np.sum(rho * g(*minus_one_one_to_r2(p1, p2, p3, p4, z)))
+    return np.sum(rho * g(*minus_one_one_to_r2(p1, p2, p3, p4, z)) * det_jac_minus_one_one_to_r2(p1, p2, p3, p4, z))
 
 
 def minus_one_one_to_r2(p1, p2, p3, p4, z):
@@ -222,7 +222,13 @@ def minus_one_one_to_r2(p1, p2, p3, p4, z):
                      + np.multiply.outer(p4 - p1, (1 - z[:, 0]) * (1 + z[:, 1])))
 
 
-def get_points_and_weights_quad_2D_rec(nq_x, nq_y):
+def det_jac_minus_one_one_to_r2(p1, p2, p3, p4, z):
+    jac0 = ((p1 - p2 + p3 - p4) * z - p1 + p2 + p3 - p4).T
+    jac1 = (p1 * (z - 1) + z * (-p2 + p3 - p4) - p2 + p3 + p4).T
+    return 0.0625 * (jac0[0, :] * jac1[1, :] - jac1[0, :] * jac0[1, :])
+
+
+def get_points_and_weights_quad_2D_quad(nq_x, nq_y):
     """
     Get Gauss quadrature points and weighs in 2D
 
@@ -286,10 +292,10 @@ if __name__ == "__main__":
     xy = barycentric_to_r2(*p, z)
     print(xy)
 
-    p = np.array([[-2, -2],
-                  [2, -2],
+    p = np.array([[-1, -3],
+                  [2, -3],
                   [2, 2],
-                  [-2, 2]])
+                  [-1, 2]])
     area = get_area_quadrilateral(*p)
     print(area)
     z = np.array([[-1, -1],
@@ -299,6 +305,17 @@ if __name__ == "__main__":
     xy = minus_one_one_to_r2(*p, z)
     print(xy)
 
-    z, rho = get_points_and_weights_quad_2D_rec(2, 3)
+    z, rho = get_points_and_weights_quad_2D_quad(2, 3)
     print(z)
     print(rho)
+
+
+    def func1(x, y):
+        return (2 - x) * (2 - y)
+
+
+    true_a = 56.25
+    a = quadrature2D_quad(*p, func1, 2)
+    print(a, true_a, abs(true_a - a))
+
+    print(det_jac_minus_one_one_to_r2(*p, z))

scaling_of_rectangle/bilinear_rectangle/assembly.py
@@ -7,8 +7,8 @@ based on Specialization-Project-fall-2021
 import numpy as np
 import scipy.sparse as sparse
 
-from base_assembly import get_basis_coef_rec, assemble_f_local_rec, ddx_phi_rec, ddy_phi_rec
-from gauss_quadrature import get_area_quadrilateral, quadrature2D_rec
+from base_assembly import get_basis_coef_quad, assemble_f_local_rec, ddx_phi_quad, ddy_phi_quad
+from gauss_quadrature import quadrature2D_quad
 from helpers import expand_index, index_map
 
 
@@ -56,22 +56,22 @@ def assemble_ints_local(ck, p_vec):
             # [u_i1*v_j1, u_i1*v_j2, u_i2*v_j1, u_i2*v_j2]
 
             def cij_func0(x, y):
-                return ddx_phi_rec(x, y, ck, i) * ddx_phi_rec(x, y, ck, j)
+                return ddx_phi_quad(x, y, ck, i) * ddx_phi_quad(x, y, ck, j)
 
             def cij_func1(x, y):
-                return ddx_phi_rec(x, y, ck, i) * ddy_phi_rec(x, y, ck, j)
+                return ddx_phi_quad(x, y, ck, i) * ddy_phi_quad(x, y, ck, j)
 
             def cij_func2(x, y):
-                return ddy_phi_rec(x, y, ck, i) * ddx_phi_rec(x, y, ck, j)
+                return ddy_phi_quad(x, y, ck, i) * ddx_phi_quad(x, y, ck, j)
 
             def cij_func3(x, y):
-                return ddy_phi_rec(x, y, ck, i) * ddy_phi_rec(x, y, ck, j)
+                return ddy_phi_quad(x, y, ck, i) * ddy_phi_quad(x, y, ck, j)
 
             nq = 2
-            cij0 = quadrature2D_rec(*p_vec, cij_func0, nq)
-            cij1 = quadrature2D_rec(*p_vec, cij_func1, nq)
-            cij2 = quadrature2D_rec(*p_vec, cij_func2, nq)
-            cij3 = quadrature2D_rec(*p_vec, cij_func3, nq)
+            cij0 = quadrature2D_quad(*p_vec, cij_func0, nq)
+            cij1 = quadrature2D_quad(*p_vec, cij_func1, nq)
+            cij2 = quadrature2D_quad(*p_vec, cij_func2, nq)
+            cij3 = quadrature2D_quad(*p_vec, cij_func3, nq)
 
             # construct local ints
 
@@ -161,8 +161,6 @@ def assemble_a1_a2_f(n, p, tri, f_func, f_func_is_not_zero):
     # Allows for efficient O(1) access of individual elements
     # load vector
     f_load_lv = np.zeros(n2d, dtype=float)
-    print(tri)
-    print(p[tri, :])
     for nk in tri:
         # nk : node-numbers for the k'th triangle
         # the points of the triangle
@@ -172,7 +170,7 @@ def assemble_a1_a2_f(n, p, tri, f_func, f_func_is_not_zero):
         # using indexmap k = 2 * i + d, d=0 for x, 1 for y, i is the node number
         # calculate the area of the triangle
         # and basis functions coef. or Jacobin inverse
-        ck = get_basis_coef_rec(p[nk, :])
+        ck = get_basis_coef_quad(p[nk, :])
         # assemble local contributions
         ints = assemble_ints_local(ck, p[nk, :])
         # expand the index
@@ -185,10 +183,6 @@ def assemble_a1_a2_f(n, p, tri, f_func, f_func_is_not_zero):
         int22[index] += ints[3]
         int4[index] += ints[4]
         int5[index] += ints[5]
-        expanded_nk2 = expand_index(np.array([4]))
-        index2 = np.ix_(expanded_nk2, expanded_nk2)
-        print((int11 + int12 + 0.5 * (int21 + int22 + int4))[index2].A, "a1")
-        print((int11 + int12 + int5)[index2].A, "a2")
         if f_func_is_not_zero:
             f_local = assemble_f_local_rec(ck, f_func, *p[nk, :])
             f_load_lv[expanded_nk] += f_local